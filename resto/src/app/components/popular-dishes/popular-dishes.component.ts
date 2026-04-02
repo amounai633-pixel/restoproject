156166523849971927196620fe0f957748cebc84
Torkhani Imen <amounai633@gmail.com>
2026-04-02 14:44:13 -0800

    third version

---
+++ resto/src/app/components/popular-dishes/popular-dishes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-popular-dishes',
+  imports: [],
+  templateUrl: './popular-dishes.component.html',
+  styleUrl: './popular-dishes.component.scss'
+})
+export class PopularDishesComponent {
+
+}
---
+++ resto/src/app/components/popular-menu/popular-menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-popular-menu',
+  imports: [],
+  templateUrl: './popular-menu.component.html',
+  styleUrl: './popular-menu.component.scss'
+})
+export class PopularMenuComponent {
+
+}
